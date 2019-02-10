a50d67c8e1849a2453a99d20794ea9c03e1c8dfb
J Shim <shimjay1@gmail.com>
2019-02-09 23:36:21 -0500

    created Game window + 2 players that can move with wsad and udlr

---
+++ src/com/tutorial/main/Game.java
@@ -0,0 +1,106 @@
+package com.tutorial.main;
+
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics;
+import java.awt.Canvas;
+import java.awt.Color;
+
+import java.util.Random;
+
+public class Game extends Canvas implements Runnable{
+
+  private static final long serialVersionUID = 00002L;
+
+  public static final int width = 640, height = width / 12 * 9;
+
+  private Thread thread;
+  private boolean running = false;
+
+  private Random r;
+  private Handler handler;
+
+  public Game(){
+    handler = new Handler();
+    this.addKeyListener(new KeyInput(handler));
+
+    new Window(width, height, "A New Game!", this);
+
+    r = new Random();
+
+    handler.addObject(new Player(width/2-32, height/2-32, ID.Player));
+    handler.addObject(new Player(width/2+64, height/2+64, ID.Player2));
+
+  }
+
+  public synchronized void start(){
+    thread = new Thread(this);
+    thread.start();
+    running = true;
+  }
+
+  public synchronized void stop(){
+    try{
+      thread.join(); // killing off the thread
+      running = false;
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+
+  }
+
+  public void run(){
+    // frame ticks gotten online
+    long lastTime = System.nanoTime();
+    double amountOfTicks = 60.0;
+    double ns = 1000000000 / amountOfTicks;
+    double delta = 0;
+    long timer = System.currentTimeMillis();
+    int frames = 0;
+    while(running){
+      long now = System.nanoTime();
+      delta += (now - lastTime) / ns;
+      lastTime = now;
+      while(delta >= 1){
+        tick();
+        delta--;
+      }
+      if(running){
+        render();
+      }
+      frames++;
+
+      if(System.currentTimeMillis() - timer > 1000){
+        timer += 1000;
+        // System.out.println("FPS: " + frames);
+        frames = 0;
+      }
+    }
+    stop();
+  }
+
+  private void tick(){
+    handler.tick();
+  }
+
+  private void render(){
+    BufferStrategy bs = this.getBufferStrategy();
+    if(bs == null){
+      this.createBufferStrategy(3);
+      return;
+    }
+
+    Graphics g = bs.getDrawGraphics();
+
+    g.setColor(Color.black);
+    g.fillRect(0, 0, width, height);
+
+    handler.render(g);
+
+    g.dispose();
+    bs.show();
+  }
+
+  public static void main(String[] args){
+    new Game();
+  }
+}
---
+++ src/com/tutorial/main/Player.java
@@ -0,0 +1,25 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player extends GameObject {
+  public Player(int x, int y, ID id){
+    super(x, y, id);
+
+  }
+
+  @Override
+  public void tick(){
+    x += velX;
+    y += velY;
+  }
+
+  @Override
+  public void render(Graphics g){
+    if(id == ID.Player) g.setColor(Color.white);
+    if(id == ID.Player2) g.setColor(Color.red);
+    g.fillRect(x, y, 32, 32);
+  }
+
+}
